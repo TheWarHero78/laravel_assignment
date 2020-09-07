8c13ce146b735fe13455ba5e3a7792c80f02ac68
TheWarHero78 <54692481+TheWarHero78@users.noreply.github.com>
2020-09-07 12:57:54 +0530

    login and registration

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>About Us</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<!--Bootsrap 4 CDN-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="{{url('style.css')}}">
+
+</head>
+<body>
+<div class="container-fluid">
+  <div class="row no-gutter">
+    <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+    <div class="col-md-8 col-lg-6">
+      <div class="login d-flex align-items-center py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
+              <h3 class="login-heading mb-4">About Us!!</h3>
+              <div class="card">
+                  <div class="card-body">                  
+                    The War Hero Solutions
+                  </div>
+                  <div class="card-body">
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione voluptates sunt neque mollitia quos, quam perspiciatis enim eligendi blanditiis perferendis aliquam, quidem voluptatibus officiis minus quod obcaecati sed corrupti omnis?
+                 Ad neque sequi ipsum, laborum vel velit officiis iusto animi saepe, facilis id? Magni vitae qui laborum similique, hic consequuntur, sed, repellat perspiciatis accusantium quos blanditiis facilis non fugiat veniam!
+                 Excepturi harum praesentium iste culpa cumque optio corrupti aliquid! Aut enim numquam quod sapiente labore vitae, aliquam, vero deserunt placeat aperiam quos consequatur quis, voluptatem modi animi perferendis earum suscipit.                     
+                 <br>
+                      <a class="small" href="{{url('dashboard')}}">Dashboard</a><br>                                                            
+                  </div>
+                  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
